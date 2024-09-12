2fda5a0f7a9b7c03225ce50e9f687be3cb5749fb
Alexandr <penkov-sasha@mail.ru>
2024-09-12 09:14:56 +0300

    Комментарий

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        float b = 5;
+        char c = '.';
+        double d = a+b;
+        boolean s = a < b;
+
+        System.out.println("Ответ: "+s+". Задача решена!");
+        }
+    }
